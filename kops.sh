7832379ce50c1c69d27049326aa250b42c047a78
Kish9669 <163889606+Kish9669@users.noreply.github.com>
2025-02-28 09:39:46 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,7 +14,7 @@ mv kops-linux-amd64 /usr/local/bin/kops
 aws s3api create-bucket --bucket kish-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 aws s3api put-bucket-versioning --bucket kish-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://kish-kops-testbkt123.k8s.local
-kops create cluster --name kish.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops create cluster --name kish.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 kops update cluster --name kish.k8s.local --yes --admin
 
 #kops validate cluster --wait 10m
